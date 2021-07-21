08ca4798fc4f8004eae83c72acb39561f4004a4f
Virgile Joinville <virgilejoinville@gmail.com>
2021-07-21 16:02:51 +0200

    Add a lot a feature, like sorting by comments, author on a Post or Comment by Users.

--- graphql-course/graphqk-basics/src/index.js
+++ graphql-course/graphqk-basics/src/index.js
@@ -1,43 +1,68 @@
 const { GraphQLServer } = require('graphql-yoga')
  
 // Demo user data
+const comments = [{
+  id: '1',
+  text: 'Awesome article, i read it many times',
+  author: '1',
+  post: '1'
+}, {
+  id : '2',
+  text: 'Not the best article ever read, but it\s ok ',
+  author: '2',
+  post: '2'
+}, {
+  id : '3',
+  text: 'I definetly prefer front end',
+  author:'3',
+  post: '3'
+}, {
+  id: '4',
+  text: 'hope you will give to us other article like this ',
+  author: '3',
+  post: '1'
+}]
 
 const users = [{
   id: '1',
   name: 'Virgile',
   email: 'virgile@example.com',
-  age: 30
+  age: 30,
 }, {
   id: '2',
   name: 'Bruno',
-  email: 'bruno@example.com'
+  email: 'bruno@example.com',
 }, {
   id:'3',
   name: 'Yann',
   email: 'yann@example.com',
-  age: 31
+  age: 31,
 }]
 
 const posts = [{
   id: '1',
   title: 'Découverte de graphQl',
   body: 'lorem ipsum dolor sit amet, consectet',
-  published: false
+  published: false,
+  author: '1'
 }, {
   id: '2',
   title: 'la phase caché de postgresQL',
   body: 'lorem ipsum dolor sit amet, consectet',
-  published: true
+  published: true,
+  author: '1'
 },{
   id: '3',
   title: 'Signature d\'un cdi dans ma boite',
   body: 'lorem ipsum dolor sit amet, consectet',
-  published: false
+  published: false,
+  author: '3'
 }]
 
 // Type Definitions
 const typeDefs = `
   type Query {
+    comments(query: String): [Comment!]!
     users(query: String): [User!]!
     posts(query: String): [Post!]!
     me: User!
@@ -45,22 +70,42 @@ const typeDefs = `
   }
 
   type User {
-      id: ID!
-      name: String!
-      email : String!
-      age: Int
+    id: ID!
+    name: String!
+    email : String!
+    age: Int
+    posts: [Post!]!
+    comments: [Comment]!
   }
   type Post{
-      id: ID!
-      title: String!
-      body: String!
-      published: Boolean!
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+    author: User!
+    comments: [Comment]!
+  }
+  type Comment{
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
   }
 `
  
 // Resolvers
 const resolvers = {
   Query: {
+    comments: (parent, args, ctx, info) => {
+      // return comments
+      if(!args.query){
+        return comments
+      }
+      return comments.filter((comment) => {
+        const isTextMatch = comment.text.toLowerCase().includes(args.query.toLowerCase())
+        return isTextMatch
+      })
+    },
     posts: (parent, args, ctx, info) => {
       if(!args.query){
         return posts
@@ -99,6 +144,42 @@ const resolvers = {
         }
     }
   },
+  Post: {
+    author: (parent, args, ctx, info) => {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    },
+    comments: (parent, args, ctx, info) => {
+      return comments.filter((comment) => {
+        return comment.post === parent.id
+      })
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info){
+      return posts.filter((post) => {
+        return post.author === parent.id
+      })
+    },
+    comments(parent, args, ctx, info){
+      return comments.filter((comment)=> {
+        return comment.author === parent.id
+      })
+    }
+  },
+  Comment: {
+    author: (parent, args, ctx, info) => {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    },
+    post: (parent, args, ctx, info) => {
+      return posts.find((post) => {
+        return post.id === parent.id
+      })
+    }
+  }
 }
  
 const server = new GraphQLServer({
